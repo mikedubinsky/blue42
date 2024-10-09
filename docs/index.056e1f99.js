5e237c2c6fe0e27777f7b74c92c13371bc4fd881
mikedubinsky <mikedubinsky24@gmail.com>
2024-10-09 19:58:08 -0400

    Push GTM

---
+++ docs/index.056e1f99.js
@@ -0,0 +1,2 @@
+var e,t,a,g,m,n;e=window,t=document,a="script",e[g="dataLayer"]=e[g]||[],e[g].push({"gtm.start":new Date().getTime(),event:"gtm.js"}),m=t.getElementsByTagName(a)[0],(n=t.createElement(a)).async=!0,n.src="https://www.googletagmanager.com/gtm.js?id=GTM-M5ZWGQFJ",m.parentNode.insertBefore(n,m);
+//# sourceMappingURL=index.056e1f99.js.map
