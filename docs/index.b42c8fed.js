41a4d97f335a51a60c90a50120def194dbe34314
mikedubinsky <mikedubinsky24@gmail.com>
2024-10-16 22:17:52 -0400

    push docs

---
+++ docs/index.b42c8fed.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("termsAndConditions"),n=document.getElementById("openTermsModal"),t=document.querySelector(".close-terms-btn");n.addEventListener("click",function(n){n.preventDefault(),e.style.display="block"}),t.addEventListener("click",function(){e.style.display="none"}),window.addEventListener("click",function(n){n.target===e&&(e.style.display="none")});
+//# sourceMappingURL=index.b42c8fed.js.map
